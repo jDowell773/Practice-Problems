0df79cb314400f730f04243e6f300beab9ca3e24
JonathanDowell <mr.jonathandowell@gmail.com>
2020-07-07 13:28:20 -0500

    completed

--- variable_to_string.rb
+++ variable_to_string.rb
@@ -41,3 +41,52 @@ color3 = "purple"
 
 puts "I'm not too fond of #{color1}, #{color2}, or #{color3}."
 
+
+# 6
+
+puts "Please enter a name: "
+name = gets.chomp
+
+if name != "Santa"
+  puts "You're not Santa."
+end
+
+
+# 7
+
+book_title = "How To Be Me"
+book_author = "Me"
+
+puts "The book " + book_title + " was written by " + book_author + "."
+
+
+# 8 
+
+book_title = "2020 Is My Year"
+book_author = "The Rona"
+
+puts "The book #{book_title} was written beautifully by #{book_author}."
+
+
+# 9
+
+puts "Please enter a password: "
+password = gets.chomp
+
+if password == "Joshua" 
+  puts "Shall we play a game?"
+else
+  puts "Access denied"
+end
+
+
+# 10 
+
+city1 = "Accra"
+city2 = "Chang Mai"
+city3 = "Paris"
+
+puts "Three cities I've visited in the last year are " + city1 + ", " + city2 + ", and " + city3 + "."
+
+
+

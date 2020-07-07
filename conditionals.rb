c5b5720635e6e64f2eb6240459f213cdc7ce40ce
JonathanDowell <mr.jonathandowell@gmail.com>
2020-07-07 09:02:29 -0500

    conditional practice

---
+++ conditionals.rb
@@ -0,0 +1,129 @@
+# 1
+
+x = 66
+
+if x == 10
+  p 0
+else
+  p -1
+end
+
+
+# 2
+
+number = 100
+
+if number < 10
+  p -1
+elsif number > 10
+  p 1
+else
+  p 0
+end
+
+
+# 3
+
+number1 = 7
+number2 = 4
+
+if number1 < 10 && number2 < 10
+  p 1
+else
+  p 0
+end
+
+
+# 4
+
+number = 40
+
+if number > 9000
+  p 1
+else
+  p -1
+end
+
+
+# 5
+
+number = 99
+
+if number < 10
+  p 9
+elsif number < 20
+  p 19
+elsif number < 30
+  p 29
+else
+  p -1
+end
+
+
+# 6
+
+number1 = 1
+number2 = 100
+
+if number1 > 10 || number2 > 10
+  p 100
+else
+  p -100
+end
+
+
+# 7
+
+number = 6
+
+if number < 0
+  p 1776
+else
+  p 1979
+end
+
+
+# 8
+
+number = 1001
+
+if number == 100
+  p 100
+elsif number == 99
+  p 99
+else
+  p 0
+end
+
+
+# 9
+
+number1 = 6
+number2 = 66
+
+if number1 < 0 && number2 > 0
+  p 1
+else
+  p 0
+end
+
+
+# 10 
+
+number = 420
+
+if number > 80
+  p 5
+elsif number > 60
+  p 4
+elsif number > 40
+  p 3
+elsif number > 20
+  p 2
+else
+  p 1
+end
+
+
+
+

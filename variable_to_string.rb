3b63b52f3e076d1dabfa62ab8b0de12dec43d9fb
JonathanDowell <mr.jonathandowell@gmail.com>
2020-07-07 13:09:48 -0500

    finished 1st 5

---
+++ variable_to_string.rb
@@ -0,0 +1,43 @@
+# 1
+
+first_name = "John"
+last_name = "Doe"
+
+p first_name + " " + last_name
+
+
+# 2
+
+first_name = "Kobe"
+last_name = "Bryant"
+
+p "#{first_name} #{last_name}"
+
+
+# 3
+
+puts "Enter 1 word: "
+word = gets.chomp
+
+if word == "marco"
+  puts "polo"
+end
+
+
+#4 
+
+color1 = "black"
+color2 = "green"
+color3 = "red"
+
+puts "I like the colors " + color1 + ", " + color2 + ", and " + color3 + "."
+
+
+# 5
+
+color1 = "orange"
+color2 = "yellow"
+color3 = "purple"
+
+puts "I'm not too fond of #{color1}, #{color2}, or #{color3}."
+

840c73e41789dbbd424ea9ffcbe9fc8354229ecb
JonathanDowell <mr.jonathandowell@gmail.com>
2020-07-08 23:47:20 -0500

    methods completed

--- methods.rb
+++ methods.rb
@@ -40,3 +40,50 @@ number = gets.chomp
 
 p number.to_i * 10
 
+
+# 6
+
+puts "Please enter 2 words: "
+word1 = gets.chomp
+word2 = gets.chomp
+
+p word1.upcase + word2.upcase
+
+
+# 7 
+
+puts "Please enter a word: "
+word = gets.chomp
+
+p word.length
+
+
+# 8
+
+puts "Please enter a number: "
+number = gets.chomp
+
+if number.to_i < 0
+  puts "That's a negative number"
+end
+
+
+# 9
+
+puts "Please enter 2 numbers: "
+number1 = gets.chomp
+number2 = gets.chomp
+
+p number1.to_i * number2.to_i
+
+
+# 10
+
+puts "Please enter a word: "
+word = gets.chomp
+
+if word.length > 5
+  puts "That's a long word"
+end
+
+

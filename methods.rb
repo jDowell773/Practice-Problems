2d4f0ef0b13634791eedb5fc6598b44a9b232d45
JonathanDowell <mr.jonathandowell@gmail.com>
2020-07-08 23:35:54 -0500

    first half of methods

---
+++ methods.rb
@@ -0,0 +1,42 @@
+# 1
+
+puts "Please enter a word: "
+word = gets.chomp
+
+puts word.upcase
+
+
+# 2
+
+puts "Please enter a number: "
+number = gets.chomp
+
+if number.to_i > 100
+  puts "That's a big number"
+end
+
+
+# 3
+
+puts "Please enter 2 numbers: "
+number1 = gets.chomp
+number2 = gets.chomp
+
+p number1.to_i + number2.to_i
+
+
+# 4
+
+puts "Please enter a word: "
+word = gets.chomp
+
+puts word.reverse
+
+
+# 5
+
+puts "Please enter a number: "
+number = gets.chomp
+
+p number.to_i * 10
+

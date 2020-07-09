1556d26d332df6a4960ac27e5213bfaf69276778
JonathanDowell <mr.jonathandowell@gmail.com>
2020-07-09 01:05:33 -0500

    loops; first half

---
+++ loops.rb
@@ -0,0 +1,54 @@
+# 1
+
+number = 1
+
+while number <= 10
+  p number
+  number += 1
+end
+
+
+# 2 
+
+number = 0
+
+while number < 5
+  puts "hello"
+  number += 1
+end
+
+
+# 3
+
+while true
+  puts "Please enter a word: "
+  word = gets.chomp
+  if word == "stop"
+    break
+  end
+end
+
+
+# 4
+
+number = 0
+
+while number <= 100
+  puts number
+  number += 5
+end
+
+
+# 5 
+
+number = 0
+
+while number < 10
+  puts 9000
+  number += 1
+end
+
+
+
+
+

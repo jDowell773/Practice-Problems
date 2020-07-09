b9c998a05b75aef81105fe76ac5a77e63ad1e96e
JonathanDowell <mr.jonathandowell@gmail.com>
2020-07-09 01:21:35 -0500

    loops completed

--- loops.rb
+++ loops.rb
@@ -49,6 +49,60 @@ while number < 10
 end
 
 
+# 6
+
+while true
+  puts "Please enter a number: "
+  number = gets.chomp
+  if number.to_i > 10
+    break
+  end
+end
+
+
+# 7
+
+number = 50
+
+while number <= 70
+  p number
+  number += 1
+end
+
+
+# 8
+
+number = 1
+
+while number <= 144
+  puts "Around the world"
+  number += 1
+end
+
+
+# 9 
+
+while true
+  puts "Please enter a word: "
+  word = gets.chomp
+  if word.length > 5
+    break
+  end
+end
+
+
+# 10 
+
+number = 2
+
+while number <= 40
+  p number
+  number += 2
+end
+
+
+
+
 
 
 

abb7f6643034a0c1b2b18924ed02809b972fff9f
Jonathan Dowell <mr.jonathandowell@gmail.com>
2020-07-12 10:43:18 -0500

    function practice completed

---
+++ functions.rb
@@ -0,0 +1,89 @@
+# 1 
+
+def times_two(number)
+  return number * 2
+end
+
+p times_two(20)
+
+
+# 2 
+
+def make_capital(string)
+  return string.upcase
+end
+
+p make_capital("yikes")
+
+
+# 3
+
+def subtracted(number1, number2)
+  return number1 - number2
+end
+
+p subtracted(100, 25) 
+
+
+# 4
+
+def square(number)
+  return number * number
+end
+
+p square(6)
+
+
+# 5 
+
+def first_letter(string)
+  return string[0]
+end
+
+p first_letter("Jay")
+
+
+# 6
+
+def combine(string1, string2, string3)
+  return "#{string1} #{string2} #{string3}" 
+end
+
+p combine("that", "is", "insane")
+
+
+# 7
+
+def number_to_string(number)
+  return number.to_s
+end
+
+p number_to_string(6)
+
+
+# 8
+
+def repeat_five_times(string)
+  return string * 5 
+end
+
+p repeat_five_times("Wow")
+
+
+# 9
+
+def average(number1, number2, number3)
+  return (number1 + number2 + number3) / 3
+end
+
+p average(5, 10, 15)
+
+
+# 10
+
+def times_ten_plus_thirty(number)
+  return number * 10 + 30
+end
+
+p times_ten_plus_thirty(10)
+

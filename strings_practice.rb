5617a7dfcf67c7fe2cbd46549bcea8a98eebe9ae
Jonathan Dowell <mr.jonathandowell@gmail.com>
2020-07-22 07:41:45 -0500

    more strings practice completed

--- strings_practice.rb
+++ strings_practice.rb
@@ -42,3 +42,49 @@ color3 = "pink"
 puts "Not sure why but #{color1}, #{color2}, and #{color3} have always been odd colors to me."
 
 
+# 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+puts "Please enter a name: "
+name = gets.chomp
+
+if name != "Santa"
+  puts "Youre not Santa."
+end
+
+
+# 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+title = "Doomsday"
+author = "Me"
+
+puts "The book " + title + " written by " + author + " shall be legendary."
+
+
+# 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the #{} operator).
+
+title = "Pandemic"
+author = "2020"
+
+puts "From the looks of it, the book #{title} is being written by #{author} right before our very eyes."
+
+
+# 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+puts "Please enter password: "
+password = gets.chomp
+
+if password == "Joshua"
+  puts "Shall we play a game?"
+else
+  puts "Access denied"
+end
+
+
+# 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+city1 = "Chicago"
+city2 = "DeKalb"
+city3 = "Carbondale"
+
+puts "Even though " + city2 + " and " + city3 + " are fun college towns, everyone knows the best ciy in Illinois is " + city1 + "."
+

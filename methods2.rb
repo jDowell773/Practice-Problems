ffa1fc2d5f6d531e11101fe33c40630c811a51cb
Jonathan Dowell <mr.jonathandowell@gmail.com>
2020-07-23 13:58:33 -0500

    more methods practice completed

--- methods2.rb
+++ methods2.rb
@@ -40,3 +40,49 @@ number = gets.chomp
 
 puts number.to_i * 10
 
+
+# 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+
+puts "Please enter 2 words: "
+word1 = gets.chomp
+word2 = gets.chomp
+
+puts word1.upcase + word2.upcase 
+
+
+# 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+puts "Please enter a word: "
+word = gets.chomp
+
+puts word.length 
+
+
+# 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+puts "Please enter a number: "
+number = gets.chomp
+
+if number.to_i < 0
+  puts "That's a negative number"
+end
+
+
+# 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+
+puts "Please enter 2 numbers: "
+number1 = gets.chomp
+number2 = gets.chomp
+
+puts number1.to_i * number2.to_i
+
+
+# 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.
+
+puts "Please enter a word: "
+word = gets.chomp
+
+if word.length > 5
+  put "Thats a long word"
+end
+

8a086f0b801d6a9431fed7fe9f9bae346959c98e
Jonathan Dowell <mr.jonathandowell@gmail.com>
2020-07-27 12:33:19 -0500

    more arrays practice completed

--- arrays2.rb
+++ arrays2.rb
@@ -44,3 +44,48 @@ lower[2] = lower[2].upcase
 p lower
 
 
+# 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+
+names = ["Larry", "Curly", "Moe"]
+index = 0
+
+while index < names.length
+  p names[index]
+  index += 1
+end
+
+
+# 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+
+strings = ["no", "more"]
+strings << "cap"
+
+p strings 
+
+
+# 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+
+numbers = [10, 9, 8, 7, 6]
+numbers[0] = numbers[0] * 10
+
+p numbers 
+
+
+# 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+numbers = [50, 40]
+index = 0
+
+while index < numbers.length 
+  p numbers[index]
+  index += 1
+end
+
+
+# 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
+countries = ["China", "Japan", "Thailand"]
+countries << "North Korea"
+
+p countries
+

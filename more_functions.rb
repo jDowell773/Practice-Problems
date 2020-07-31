64908ad586c7afee368b6ceb0f7f4c571b7fe513
Jonathan Dowell <mr.jonathandowell@gmail.com>
2020-07-30 21:15:33 -0500

    more functions practice

---
+++ more_functions.rb
@@ -0,0 +1,47 @@
+# 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
+
+def times_two(number)
+  return number * 2
+end
+
+p times_two(20)
+
+
+# 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+
+def big_cap(string)
+  return string.upcase
+end
+
+p big_cap("wowzers")
+
+
+# 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
+
+def subtracted(number1, number2)
+  return number1 - number2
+end
+
+p subtracted(100, 50)
+
+
+# 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+
+def squared(number)
+  return number * number
+end
+
+p squared(6)
+
+
+# 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
+
+def first_letter(string)
+  return string[0]
+end
+
+p first_letter("applesauce")
+
+
+
+

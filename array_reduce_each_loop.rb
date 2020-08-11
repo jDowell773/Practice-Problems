6428b65a3545cce24379d70d21a68a257b60adf4
Jonathan Dowell <mr.jonathandowell@gmail.com>
2020-08-10 21:10:16 -0500

    array reduce each loop practice complete

--- array_reduce_each_loop.rb
+++ array_reduce_each_loop.rb
@@ -68,26 +68,70 @@ p length
 #  6. Start with an array of hashes and find the hash with the lowest price (from the :price key).
 #  For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "pencil", price: 1}.
 
+hashes = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+lowest_price = hashes[0]
 
+hashes.each do |hash|
+  if hash[:price] < lowest_price[:price]
+    lowest_price = hash
+  end
+end
+
+p lowest_price
 
 
 #  7. Start with an array of numbers and compute product of all the numbers.
 #  For example, [5, 10, 8, 3] becomes 1200.
 
+numbers = [5, 10, 8, 3]
+product = 1
 
+numbers.each do |number|
+  product = product * number
+end
+
+p product
 
 
 #  8. Start with an array of strings and combine them all into a single string, separated by dashes.
 #  For example, ["volleyball", "basketball", "badminton"] becomes "-volleyball-basketball-badminton-".
 
+strings = ["volleyball", "basketball", "badminton"]
+single_string = "-"
 
+strings.each do |string|
+  single_string += "#{string}-"
+end
+
+p single_string
 
 
 #  9. Start with an array of hashes and find the hash with the shortest name (from the :name key).
 #  For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes {name: "book", price: 4}.
 
+hashes = [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}]
+shortest = hashes[0]
 
+hashes.each do |hash|
+  if hash[:name].length < shortest[:name].length 
+    shortest = hash
+  end
+end
+
+p shortest
 
 
 # 10. Start with an array of numbers and compute the maximum number.
 #  For example, [5, 10, 8, 3] becomes 10.
+
+numbers = [5, 10, 8, 3] 
+max_number = 0
+
+numbers.each do |number|
+  if number > max_number
+    max_number = number
+  end
+end
+
+p max_number
+

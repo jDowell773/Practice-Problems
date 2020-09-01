e8bb2930d4ada95a01abd4e149f54c2ef27f91b6
Jonathan Dowell <mr.jonathandowell@gmail.com>
2020-09-01 14:48:02 -0500

    more methods practice

---
+++ more_methods.rb
@@ -0,0 +1,60 @@
+# 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
+
+puts "Please enter a word: "
+word = gets.chomp
+
+puts word.upcase 
+
+
+# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+puts "Pease enter a number: "
+number = gets.chomp
+
+if number.to_i > 100
+  p "That's a big number"
+end
+
+
+# 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+
+puts "Please enter 2 numbers: "
+number1 = gets.chomp
+number2 = gets.chomp 
+
+puts number1.to_i + number2.to_i 
+
+
+# 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
+
+puts "Please enter a word: "
+word = gets.chomp 
+
+puts word.reverse 
+
+
+# 5. Write a program that asks the user to enter a number, then prints the number times 10.
+
+puts "Please enter a number: "
+number = gets.chomp
+
+puts number.to_i * 10
+
+
+# 6. Write a program that asks the user to enter two words, then prints both words on the same line in all capital letters.
+
+
+
+# 7. Write a program that asks the user to enter a word, then prints the number of letters in the word.
+
+
+
+# 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
+
+
+
+# 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+
+
+
+# 10. Write a program that asks the user to enter a word, then prints "That's a long word" if the word has more than 5 letters.

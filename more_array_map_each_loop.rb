ee769d3914ebf63e2872e3ef524667fa67e92cf3
Jonathan Dowell <mr.jonathandowell@gmail.com>
2020-09-07 22:59:33 -0500

    array map each loop practice completed

--- more_array_map_each_loop.rb
+++ more_array_map_each_loop.rb
@@ -69,22 +69,69 @@ p lengths
 #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 #  For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
 
+hashes = [
+  {name: "Alice", age: 27},
+   {name: "Blane", age: 16}
+  ]
+numbers = []
+
+hashes.each do |hash|
+  numbers << hash[:age]
+end
+
+p numbers
 
 
 #  7. Start with an array of numbers and create a new array with each number divided by 2.
 #  For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
 
+numbers = [1, 2, 3]
+new_numbers = []
+
+numbers.each do |number|
+  new_numbers << number / 2.0
+end
+
+p new_numbers
 
 
 #  8. Start with an array of strings and create a new array with each string's first letter only.
 #  For example, ["hello", "goodbye"] becomes ["h", "g"].
 
+strings = ["hello", "goodbye"]
+first_letter = []
+
+strings.each do |string|
+  first_letter << string[0]
+end
+
+p first_letter
 
 
 # 9.  Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
 #  For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
 
+hashes = [
+  {name: "Alice", age: 27},
+   {name: "Blane", age: 16}
+  ]
+numbers = []
+
+hashes.each do |hash|
+  numbers << hash[:age] * 2
+end
+
+p numbers
 
 
 # 10. Start with an array of numbers and create a new array with each number converted into a string.
 #  For example, [1, 2, 3] becomes ["1", "2", "3"].
+
+numbers = [1, 2, 3]
+number_to_string = []
+
+numbers.each do |number|
+  number_to_string << number.to_s
+end
+
+p number_to_string

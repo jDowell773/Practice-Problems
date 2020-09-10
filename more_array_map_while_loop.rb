a5f395e130121d8437a848a704a372ab75424126
Jonathan Dowell <mr.jonathandowell@gmail.com>
2020-09-09 23:00:01 -0500

    array map while loop practice complete

--- more_array_map_while_loop.rb
+++ more_array_map_while_loop.rb
@@ -81,23 +81,82 @@ p string_length
 #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 #  For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
 
+hashes = [
+  {name: "Alice", age: 27}, 
+  {name: "Blane", age: 16}
+]
+number_values = []
+index = 0
+
+while index < hashes.length
+  hash = hashes[index]
+  number_values << hash[:age]
+  index += 1
+end
+
+p number_values
 
 
 #  7. Start with an array of numbers and create a new array with each number divided by 2.
 #  For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
 
+numbers = [1, 2, 3]
+new_numbers = []
+index = 0
+
+while index < numbers.length
+  new_numbers << numbers[index] / 2.0
+  index += 1
+end
+
+p new_numbers
 
 
 #  8. Start with an array of strings and create a new array with each string's first letter only.
 #  For example, ["hello", "goodbye"] becomes ["h", "g"].
 
+strings = ["hello", "goodbye"]
+first_letter = []
+index = 0
+
+while index < strings.length
+  string = strings[index]
+  first_letter << string[0]
+  index += 1
+end
+
+p first_letter
 
 
 # 9.  Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
 #  For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
 
+hashes = [
+  {name: "Alice", age: 27}, 
+  {name: "Blane", age: 16}
+]
+number_values = []
+index = 0
+
+while index < hashes.length
+  number_values << hashes[index][:age] * 2
+  index += 1
+end
+
+p number_values 
 
 
 # 10. Start with an array of numbers and create a new array with each number converted into a string.
 #  For example, [1, 2, 3] becomes ["1", "2", "3"].
 
+numbers = [1, 2, 3]
+num_to_string = []
+index = 0
+
+while index < numbers.length
+  number = numbers[index]
+  num_to_string << number.to_s
+  index += 1
+end
+
+p num_to_string
